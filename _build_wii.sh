a60be321670edf4434cf37484e112514c47b56f3
CaptnCH <113392466+CaptnCH@users.noreply.github.com>
2023-11-29 23:16:01 -0500

    made build system 101% easier

--- _build_wii.sh
+++
@@ -1,6 +0,0 @@
-cp ./_build/wii_rebuild_files/main_wii.hdr ./_build/wii
-rm ./_build/wii/main_wii_2.ark
-dependencies/arkhelper patchcreator -a ./_ark -o ./_build/wii ./_build/wii/main_wii.hdr
-mv ./_build/wii/gen/main_wii.hdr ./_build/wii
-mv ./_build/wii/gen/main_wii_2.ark ./_build/wii
-rmdir ./_build/wii/gen
